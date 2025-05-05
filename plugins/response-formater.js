260139960cc27ec23d500a7dad9f492d3fb6f71a
Станіслав Кобзар <sp.erydit@gmail.com>
2025-05-05 23:41:53 +0300

    add response format

---
+++ plugins/response-formater.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import fp from "fastify-plugin";
+
+export default fp(async function (fastify, opts) {
+  fastify.addHook("onSend", (request, reply, payload, done) => {
+    const err = null;
+    const success = reply.statusCode >= 200 && reply.statusCode < 300
+    done(err, JSON.stringify({
+        success,
+        data: JSON.parse(payload),
+    }));
+});
+});
