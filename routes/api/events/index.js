050f39cf47168987656e6a152aaff4205458574a
Станіслав Кобзар <sp.erydit@gmail.com>
2025-05-16 03:51:34 +0300

    add get event endpoint

--- routes/api/events/index.js
+++ routes/api/events/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-import { createEvent } from "../../../controllers/event.js";
+import { Type } from "@sinclair/typebox";
+import { createEvent, getEventList } from "../../../controllers/event.js";
 import { PostEventsSchema } from "../../../validation/events.js";
 
 /**
@@ -19,7 +20,34 @@ export default async function (fastify, opts) {
     },
     async function (request, reply) {
       const id = await createEvent(request.body, request.user);
-      reply.send({ });
+      reply.send({});
+    }
+  );
+
+  fastify.get(
+    "/",
+    {
+      preHandler: [fastify.groupAuthenticate],
+      schema: {
+        querystring: Type.Partial(
+          Type.Object({
+            topicId: Type.String({ format: "uuid" }),
+            before: Type.String(),
+            after: Type.String(),
+          })
+        ),
+      },
+    },
+    async function (request, reply) {
+      const eventList = await getEventList(
+        {
+          topicId: request.query.topicId,
+          before: new Date(request.query.before),
+          after: new Date(request.query.after),
+        },
+        request.user
+      );
+      reply.send(eventList);
     }
   );
 }
