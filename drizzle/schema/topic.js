0966f3fdc3faf9a87bce28328f8beddb2460e4b1
Станіслав Кобзар <sp.erydit@gmail.com>
2025-05-19 11:07:10 +0300

    save

--- drizzle/schema/topic.js
+++ drizzle/schema/topic.js
@@ -3,7 +3,7 @@ import { groups } from "./groups.js";
 
 export const topics = pgTable("topics", {
   id: uuid().primaryKey().notNull(),
-  name: varchar("name").notNull(),
+  name: varchar("name", { length: 50 }).notNull(),
   description: varchar("description"),
   created: timestamp().notNull(),
   groupId: uuid("group_id").notNull().references(() => groups.id),
